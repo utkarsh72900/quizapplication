024dc675ca44d9192307bedecaf16e46fc1f71de
utkarsh72900 <utkarsh72900@gmail.com>
2024-12-20 22:30:14 +0530

    initial commit

--- src/quiz/application/Rules.java
+++ src/quiz/application/Rules.java
@@ -25,14 +25,9 @@ public class Rules extends JFrame implements ActionListener{
         rules.setFont(new Font("Tahoma", Font.PLAIN, 16));
         rules.setText(
             "<html>"+ 
-                "1. You are trained to be a programmer and not a story teller, answer point to point" + "<br><br>" +
-                "2. Do not unnecessarily smile at the person sitting next to you, they may also not know the answer" + "<br><br>" +
-                "3. You may have lot of options in life but here all the questions are compulsory" + "<br><br>" +
-                "4. Crying is allowed but please do so quietly." + "<br><br>" +
-                "5. Only a fool asks and a wise answers (Be wise, not otherwise)" + "<br><br>" +
-                "6. Do not get nervous if your friend is answering more questions, may be he/she is doing Jai Mata Di" + "<br><br>" +
-                "7. Brace yourself, this paper is not for the faint hearted" + "<br><br>" +
-                "8. May you know more than what John Snow knows, Good Luck" + "<br><br>" +
+                "1. Answer point to point" + "<br><br>" +
+                "2. Do not unnecessarily attempt any option" + "<br><br>" +
+              
             "<html>"
         );
         add(rules);
